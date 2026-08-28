b2b8eb9aa1846dd328dc25b2ee9ddf6e1490ad83
chauquocanh1998-crypto <chauquocanh1998@gmail.com>
2026-08-28 10:44:07 +0700

    .

--- app.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "kiem-ke-noi-bo.firebaseapp.com",
-  projectId: "kiem-ke-noi-bo",
-  storageBucket: "kiem-ke-noi-bo.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcde"
-};
